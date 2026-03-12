9e46e55bf892fab328808c5e0de46587722c2203
Shadow-XX <mazenasad63@gmail.com>
2026-03-12 17:20:14 +0300

    Update store.sh

--- store.sh
+++ store.sh
@@ -10,22 +10,20 @@ echo -e "[05] DNS Changer     [10] Root Checker"
 
 echo -en "\n${R}SELECT >> ${N}"
 read s_opt
-echo -en "Choose Name: "; read c_name
+echo -en "App Name: "; read c_name
 echo -en "Icon Link: "; read c_icon
 
-echo -e "${G}[*] Building... Please wait (80MB Generating)${N}"
-mkdir -p build/assets build/lib/armeabi-v7a build/META-INF build/res/drawable
+echo -e "${G}[*] Building 80MB Signed Package...${N}"
+mkdir -p build/assets build/lib/armeabi-v7a build/META-INF
 
-# إضافة كود بايثون ضخم (أكثر من 500 سطر وهمي)
-echo "print('Starting Engine...')" > build/assets/main.py
-for i in {1..500}; do echo "print('Loading Module $i...')" >> build/assets/main.py; done
-
-# الملف الثقيل (80 ميجا)
-dd if=/dev/urandom of=build/lib/armeabi-v7a/libengine.so bs=1M count=80 > /dev/null 2>&1
-curl -s -L "$c_icon" -o build/res/drawable/icon.png
-touch build/resources.arsc build/classes.dex
+# ملف الـ 80 ميجا
+dd if=/dev/zero of=build/lib/armeabi-v7a/libengine.so bs=1M count=80 > /dev/null 2>&1
+curl -s -L "$c_icon" -o build/icon.png
+touch build/classes.dex build/resources.arsc
 echo '<?xml version="1.0" encoding="utf-8"?><manifest xmlns:android="http://schemas.android.com/apk/res/android" package="com.mazen.store.'$s_opt'"><uses-sdk android:minSdkVersion="21" /></manifest>' > build/AndroidManifest.xml
 
-cd build && zip -r "../xxapk/${c_name}.apk" . > /dev/null 2>&1 && cd ..
-rm -rf build
-echo -e "${G}[SUCCESS] APK Ready! Size: 80MB${N}"; sleep 2
+cd build && zip -r "../xxapk/tmp.apk" . > /dev/null 2>&1 && cd ..
+apksigner debug "xxapk/tmp.apk" "xxapk/${c_name}.apk" > /dev/null 2>&1
+rm -rf build "xxapk/tmp.apk"
+
+echo -e "${G}[SUCCESS] Signed APK Ready! Size: 80MB${N}"; sleep 2
